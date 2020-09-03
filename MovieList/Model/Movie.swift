7c6eb6d0951bb5664f0615312a148269f3e6a858
IngaKirsona <ingas@inbox.lv>
2020-09-03 13:42:41 +0300

    data manager

---
+++ MovieList/Model/Movie.swift
@@ -0,0 +1,20 @@
+//
+//  Movie.swift
+//  MovieList
+//
+//  Created by Inga Kirsona on 03/09/2020.
+//  Copyright © 2020 Inga Kirsona. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    let movie: String
+    let movieYear: String
+    let poster: String
+    
+    static func createMovie() -> [Movie]{
+        var movies: [Movie] = []
+        return movies
+    }
+}
