e4f7a76515c1558f72bb8b806de693ff37676dcf
IngaKirsona <ingas@inbox.lv>
2020-09-07 10:19:37 +0300

    info also added

--- MovieList/Controller/InfoViewController.swift
+++ MovieList/Controller/InfoViewController.swift
@@ -10,21 +10,23 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
+    @IBOutlet weak var appInfoLabel: UILabel!
+       
+        let appInfoText = "This is my homework project.\n Check the music and my favorite movies!"
+            
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+                super.viewDidLoad()
+                appInfoLabel.text = appInfoText
+                if !appInfoText.isEmpty{
+                    appInfoLabel.text = appInfoText
+                }
+            }
+            
+            @IBAction func closeButtonTapped(_ sender: Any) {
+                self.dismiss(animated: true, completion: nil)
+                print("Dismiss View Controller")
+            }
+            
+        }
